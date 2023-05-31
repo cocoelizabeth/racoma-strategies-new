ed1c450948d3e0d2135e93f2444b910b0d18414f
cocoelizabeth <cocoelizabeth89@gmail.com>
2023-05-31 00:01:41 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,12 @@ module.exports = {
       "projectId": "jpg1p17m",
       "dataset": "production"
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+  }, "gatsby-plugin-image", 
+  "gatsby-plugin-sharp", 
+  "gatsby-transformer-sharp", 
+  "gatsby-plugin-styled-components", 
+  // "gatsby-plugin-google-gtag", 
+  "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
