908f4783b8c36d9e3fea44657b414c1c881203c1
cocoelizabeth <cocoelizabeth89@gmail.com>
2023-06-04 23:36:25 -0400

    deleted commented text

--- web/src/styles/typography/ParagraphTextStyles.js
+++ web/src/styles/typography/ParagraphTextStyles.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
 
 export const ParagraphTextStyles = styled.p`
-  ${'' /* font-family: 'Inter', sans-serif;
-  color: var(--grey);
-  font-size: 1.6rem;
-  line-height: 1.5; */}
+ 
 
   @media only screen and (max-width: 768px) {
-    ${'' /* font-size: 1.4rem; */}
+  
   }
 `;
