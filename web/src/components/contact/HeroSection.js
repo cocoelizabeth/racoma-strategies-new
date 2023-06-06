db655e2b66906e1d1db49c561c7d71fffc99758d
cocoelizabeth <cocoelizabeth89@gmail.com>
2023-06-06 13:36:34 -0400

    pulling content from homepage contect section

--- web/src/components/contact/HeroSection.js
+++ web/src/components/contact/HeroSection.js
@@ -1,47 +1,49 @@
-import React, { useRef, useEffect } from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
-import { HeroSectionStyles } from '../../styles/contact/HeroSectionStyles';
-import MyPortableText from '../MyPortableText';
-import Button from '../buttons/Button';
-import { socialLinks } from '../../constants/socialLinks';
+import React, { useRef, useEffect } from "react";
+import { Link, graphql, useStaticQuery } from "gatsby";
+import { HeroSectionStyles } from "../../styles/contact/HeroSectionStyles";
+import MyPortableText from "../MyPortableText";
+import Button from "../buttons/Button";
+import { socialLinks } from "../../constants/socialLinks";
 
 function HeroSection() {
   const data = useStaticQuery(graphql`
     {
       allSanityHome {
         nodes {
-          hero {
-            headlineText
-            _rawSubHeadlineText
-            cta
+          contact {
+            headerText
+            _rawSubText
             link
+            cta
+            _key
           }
         }
       }
     }
   `);
 
-  const { hero } = data.allSanityHome.nodes[0];
+  const { contact } = data.allSanityHome.nodes[0];
 
   return (
     <HeroSectionStyles>
       <section>
         <div className="hero__wrapper">
-          <div className="headlineText h2">GET IN TOUCH</div>
+          <div className="headlineText h2">{contact.headerText}</div>
           <div className="subheadingText">
-            <div className="subheadingText__text">
-              Let’s talk about your transition goals and how Racoma Strategies
-              can make them become a reality. We’d love to connect.
+            <div className="subheadingText">
+              <MyPortableText
+                className="subheadingText"
+                value={contact._rawSubText}
+              />
             </div>
 
-              <ul className="contactLinks">
-                {socialLinks.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.url}>{item.text}</a>
-                  </li>
-                ))}
-              </ul>
-        
+            <ul className="contactLinks">
+              {socialLinks.map((item) => (
+                <li key={item.name}>
+                  <a href={item.url}>{item.text}</a>
+                </li>
+              ))}
+            </ul>
 
             {/* <span className="placeholder" /> */}
             {/* <Button to={hero.link} tag={Link} className="heroCTA">
